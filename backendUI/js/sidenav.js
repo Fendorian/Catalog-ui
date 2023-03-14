83e366687835a9e32e92e51b2efb0f4c8316d620
Fendorian <papic.stefan24@gmail.com>
2023-03-14 13:23:16 +0000

    Small refactor, frontend ui mockup started

--- backendUI/js/sidenav.js
+++ backendUI/js/sidenav.js
@@ -1,27 +1,31 @@
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   window.setTimeout(() => {
-    let sideNavTransition = document.querySelector('.side-nav');
-    sideNavTransition.style.transform = "translateX(250px)";  
-  }, 1000); 
+    let sideNavTransition = document.querySelector(".side-nav");
+    sideNavTransition.style.transform = "translateX(250px)";
+  }, 1000);
 });
+function toggleDisplay(id) {
+  let divs = document.getElementsByClassName("content");
+  let menuItems = document.getElementsByClassName("menu-item");
+  for (var i = 0; i < divs.length; i++) {
+    if (divs[i].id == id) {
+      divs[i].style.display = "block";
+      if (id != "home") {
+        document.getElementById("home").style.display = "none";
+      }
+    } else {
+      divs[i].style.display = "none";
+    }
+  }
+  if (id == "home") {
+    document.getElementById("home").style.display = "block";
+  }
+  for (var i = 0; i < menuItems.length; i++) {
 
-function productsComponent(){
-  let productComponent = document.querySelector('.product-component');
-  productComponent.style.display = 'block';
-  console.log("Ovo je product component");
-  let hideHomeComponent = document.querySelector('.content-wrapper');
-  hideHomeComponent.style.display = 'none';
-  let selectHover = document.querySelector('.products-link');
-  selectHover.style.backgroundColor = 'green';
-};
-function showCreateForm() {
-
+    if (menuItems[i].dataset.target === id) {
+      menuItems[i].classList.add("selected");
+    } else {
+      menuItems[i].classList.remove("selected");
+    }
+  }
 }
-
-function categoryComponent(){
-  console.log("Ovo je category component");
-};
-
-function subCategoryComponent(){
-  console.log("Ovo je subcategory component");
-};
